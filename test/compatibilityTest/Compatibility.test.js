8791378316917614bb32a3530cbc84ff6fa275d3
SangHun Lee - Corp <ayteneve93@gmail.com>
2020-09-11 16:31:40 +0900

    호환성 테스트는 별도 테스트 프레임워크를 사용하지 않음

---
+++ test/compatibilityTest/Compatibility.test.js
@@ -0,0 +1,19 @@
+const {
+    /* Base */
+    Entry, KoconutEntry, Pair, KoconutPair,
+
+    /* Container */
+    KoconutArray, KoconutSet, KoconutMap,
+
+    /* Exception */
+    KoconutConflictException, KoconutNoSuchElementException, KoconutInvalidArgumentException, KoconutIndexOutOfBoundsException,
+
+    /* Enum */
+    KoconutLocale, KoconutLoopSignal,
+
+    /* Tool */
+    KoconutOption,
+
+    /* Protocol */
+    KoconutComparable, KoconutEquatable,
+} = require("../../dist/index.internal")
