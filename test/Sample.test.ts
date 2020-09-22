fa2f803743781f633de0011258f28cdae7404561
SangHun Lee - Corp <ayteneve93@gmail.com>
2020-09-22 18:08:22 +0900

    Boolean 확장 방식 고민중

--- test/Sample.test.ts
+++ test/Sample.test.ts
@@ -5,20 +5,9 @@ import {
     KoconutLocale, KoconutOption, KoconutDeprecation, KoconutComparable, KoconutMap, Entry, Pair, KoconutPair, KoconutEntry, KoconutEquatable
 } from "../lib/module.internal" // Same as -- from 'koconut'
 
+
 const sampleProcess = async () => {
-    
-    const numberKeyStringValueMap = await KoconutMap.generate(
-                                            5, i => [i, i.toString()]
-                                                    // ↑ Also can be
-                                                    //   new Pair(i, i.toString())
-                                                    //   Pair.from([i, i.toString()])
-                                                    //   new KoconutPair(i, i.toString())
-                                                    //   new Entry(i, i.toString())
-                                                    //   Entry.from([i, i.toString()])
-                                                    //   new KoconutEntry(i, i.toString())      
-                                        )
-                                        .yield()
-    console.log(numberKeyStringValueMap)
-    // ↑ Map { 0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4' }
+
 
 }
+sampleProcess()
