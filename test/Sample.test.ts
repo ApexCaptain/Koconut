ee420d04b7b2739131a144bd28a69249441c7a8b
SangHun Lee - Corp <ayteneve93@gmail.com>
2020-10-06 17:59:50 +0900

    시퀀스에 또 다른 오류 발견...

--- test/Sample.test.ts
+++ test/Sample.test.ts
@@ -9,30 +9,15 @@ import {
 const sampleProcess = async () => {
 
 
-    const rst =  await new KoconutSequence(new Sequence([1,2,3,4,5,6,7,8,9,10]))
-                        .onEachIndexed(console.log)
-                        .onEachIndexed(console.log)
-                        .onEach(console.log)
-                        .onEach(console.log)
+    const rst =  new KoconutSequence(new Sequence([1,2,3,4,5,6,7,8,9,10]))
                         
-                        /*
-                        .filter(eachNumber => new Promise(resolve => {
-                            setTimeout(() => {
-                                resolve(eachNumber % 2 == 0)
-                            }, 1000)
-                        }))
-                        .onEach(e => {
-                            if(e > 5) return KoconutLoopSignal.BREAK
-                            console.log("qwe", e)
-                        })
-                        .onEach(console.log)
-                        .maxByOrNull(e => e)
-                        */
-                        .asArray()
-                        .yield()
+    const b = rst.filter(eachNumber => eachNumber % 2 == 0)
+                    .filter(eachNumber => eachNumber % 2 == 0)
+                    .asArray()
 
-    console.log("-----------------")
-    console.log(rst)
-    console.log("-----------------")
+    console.log(b)
+    console.log(await rst.asArray().yield())
+    console.log(b)
+    console.log(await b.yield())
 }
 sampleProcess()
