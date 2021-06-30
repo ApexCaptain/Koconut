81bc1ded0eee456b45a8d78fe2a277a8a6f07156
SangHun Lee <ayteneve93@gmail.com>
2021-06-30 17:07:35 +0900

    Testing 17

--- commands/MochaDependencyMacher.ts
+++ commands/MochaDependencyMacher.ts
@@ -5,8 +5,9 @@ import {
 
 const matchMochaDependency = async () => {
     const nodeHeadVersionLevel = parseInt((await runPromisifiedCommand("node -v")).trim().split(".")[0].slice(1))
-    if(nodeHeadVersionLevel <= 7) await runPromisifiedCommand("npm install mocha@^6.x --save-dev")
-    else if(nodeHeadVersionLevel <= 9) await runPromisifiedCommand("npm install mocha@^7.x --save-dev")
+    if(nodeHeadVersionLevel < 8) await runPromisifiedCommand("npm install mocha@^6.x --save-dev")
+    else if(nodeHeadVersionLevel < 10) await runPromisifiedCommand("npm install mocha@^7.x --save-dev")
+    // else if(nodeHeadVersionLevel < 12) await runPromisifiedCommand("npm install mocha@^8.x --save-dev")
     exit(0)
 }
 matchMochaDependency()
