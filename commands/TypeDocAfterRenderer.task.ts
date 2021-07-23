e0c0649cef9f798879847f3a94d9522fcfe621a8
SangHun Lee <ayteneve93@gmail.com>
2021-07-23 14:31:35 +0900

    After Renderer

--- commands/TypeDocAfterRenderer.task.ts
+++ commands/TypeDocAfterRenderer.task.ts
@@ -1,12 +1,51 @@
 import { join, normalize } from 'path'
-import { appendFileSync, writeFileSync } from 'fs'
+import { appendFileSync, existsSync, readdirSync, statSync, writeFileSync } from 'fs'
+import * as Koconut from "../src/index.internal"
 
 const rootPath = normalize(`${__dirname}/../`)
-const nojekyllPath = join(rootPath, "docs", ".nojekyll")
-writeFileSync(nojekyllPath, "")
-const mainStyleSheetPath = join(rootPath, "docs", "assets", "css", "main.css")
-appendFileSync(mainStyleSheetPath, `
-    .tsd-generator {
-        display: none;
+
+const setNoJekyll = () => {
+    const nojekyllPath = join(rootPath, "docs", ".nojekyll")
+    writeFileSync(nojekyllPath, "")
+}
+
+const setTsdGeneratorCssInvisible = () => {
+    const mainStyleSheetPath = join(rootPath, "docs", "assets", "css", "main.css")
+    appendFileSync(mainStyleSheetPath, `
+        .tsd-generator {
+            display: none;
+        }
+    `)
+}
+
+const capitalizePageFiles = () => {
+    if(process.platform != 'win32') return
+    const srcPath = join(rootPath, "src")
+    let foundFiles = Array<string>()
+    const scanFiles = (dirPath : string) => {
+        if(existsSync(dirPath)) {
+            readdirSync(dirPath).forEach(eachFileName => {
+                const eachFilePath = normalize(join(dirPath, eachFileName))
+                if(statSync(eachFilePath).isDirectory()) scanFiles(eachFilePath)
+                else if(!eachFileName.includes("internal")) foundFiles.push(eachFileName)
+            })
+        }
     }
-`)
+    scanFiles(srcPath)
+    // foundFiles = foundFiles.map
+
+    console.log(foundFiles)
+
+    console.log(Object.values(Koconut))
+    const t= Object.values(Koconut).map((it : any) => it['name'])
+    console.log(t)
+}
+
+const typeDocAfterRendering = async () => {
+    
+    setNoJekyll()
+    setTsdGeneratorCssInvisible()
+    
+    // capitalizePageFiles()
+}
+typeDocAfterRendering()
