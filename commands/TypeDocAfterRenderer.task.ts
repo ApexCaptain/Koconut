a06a4990d036c93d75070e9f677831d7cfab4ff7
ApexCaptain <ayteneve93@gmail.com>
2021-11-04 16:40:14 +0900

    Package Updated

--- commands/TypeDocAfterRenderer.task.ts
+++ commands/TypeDocAfterRenderer.task.ts
@@ -1,27 +1,28 @@
-import { join, normalize } from 'path'
-import { appendFileSync, writeFileSync } from 'fs'
+import { join, normalize } from "path";
+import { appendFileSync, writeFileSync } from "fs";
 
-const rootPath = normalize(`${__dirname}/../`)
+const rootPath = normalize(`${__dirname}/../`);
 
 const setNoJekyll = () => {
-    const nojekyllPath = join(rootPath, "docs", ".nojekyll")
-    writeFileSync(nojekyllPath, "")
-}
-
+  const nojekyllPath = join(rootPath, "docs", ".nojekyll");
+  writeFileSync(nojekyllPath, "");
+};
 
 const setTsdGeneratorCssInvisible = () => {
-    const mainStyleSheetPath = join(rootPath, "docs", "assets", "css", "main.css")
-    appendFileSync(mainStyleSheetPath, `
+  const mainStyleSheetPath = join(rootPath, "docs", "assets", "style.css");
+  appendFileSync(
+    mainStyleSheetPath,
+    `
         .tsd-generator {
             display: none;
         }
-    `)
-}
+    `
+  );
+};
 
 const typeDocAfterRendering = () => {
+  setNoJekyll();
+  setTsdGeneratorCssInvisible();
+};
 
-    setNoJekyll()
-    setTsdGeneratorCssInvisible()
-}
-
-typeDocAfterRendering()
+typeDocAfterRendering();
