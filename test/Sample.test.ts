92d7ee1fab9ffddb9e6be490d8b95eeeaed88e96
SangHun Lee - MacBook Pro 2020 <ayteneve93@gmail.com>
2021-11-19 19:48:42 +0900

    RegCommit 2021-11-19

--- test/Sample.test.ts
+++ test/Sample.test.ts
@@ -1,15 +1,11 @@
-// ↑
-
+import { expect } from 'chai' 
 import {
     KoconutArray, KoconutSet, KoconutLoopSignal, KoconutFlow, Flow, KoconutBoolean, KoconutSequence, Sequence,
     KoconutLocale, KoconutOption, KoconutDeprecation, KoconutComparable, KoconutMap, Entry, Pair, KoconutPair, KoconutEntry, KoconutEquatable
-} from "../src/module" // Same as -- from 'koconut'
+} from "../src/module"
 
-
-const sampleProcess = async () => {
-    const sample = KoconutSequence.of(1,2,3,4,5)
-    console.log(
-        await sample.yield()
-    )
-}
-sampleProcess()
+describe('Sample Test', () => {
+    it("Test 1", async () => {
+        
+    })
+})
