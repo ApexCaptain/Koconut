cd7fa3685dae3807c5c7f9e4c625f5d0b36e778e
ApexCaptain <ayteneve93@gmail.com>
2021-11-22 16:21:42 +0900

    Test Commit

--- commands/MochaDependencyMacher.task.ts
+++ commands/MochaDependencyMacher.task.ts
@@ -1,13 +1,16 @@
-import { exit } from "process"
-import {
-    runPromisifiedCommand
-} from "./CommandShell.task"
+import { exit } from "process";
+import { runPromisifiedCommand } from "./CommandShell.task";
 
 const matchMochaDependency = async () => {
-    const nodeHeadVersionLevel = parseInt((await runPromisifiedCommand("node -v")).trim().split(".")[0].slice(1))
-    if(nodeHeadVersionLevel < 8) await runPromisifiedCommand("npm install mocha@^6.x --save-dev")
-    else if(nodeHeadVersionLevel < 10) await runPromisifiedCommand("npm install mocha@^7.x --save-dev")
-    else if(nodeHeadVersionLevel < 12) await runPromisifiedCommand("npm install mocha@^8.x --save-dev")
-    exit(0)
-}
-matchMochaDependency()
+  const nodeHeadVersionLevel = parseInt(
+    (await runPromisifiedCommand("node -v")).trim().split(".")[0].slice(1)
+  );
+  if (nodeHeadVersionLevel < 8)
+    await runPromisifiedCommand("yarn add mocha@^6.x");
+  else if (nodeHeadVersionLevel < 10)
+    await runPromisifiedCommand("yarn add mocha@^7.x");
+  else if (nodeHeadVersionLevel < 12)
+    await runPromisifiedCommand("yarn add mocha@^8.x ");
+  exit(0);
+};
+matchMochaDependency();
