cbbfbbcca43adf5d05cdbb3b567cd571c4d90900
ApexCaptain <ayteneve93@gmail.com>
2021-11-22 10:57:57 +0900

    RegCommit 2021-11-22

--- commands/LocalDocumentViewer.task.ts
+++ commands/LocalDocumentViewer.task.ts
@@ -1,7 +1,8 @@
-import express from 'express'
-import open from 'open'
-const app : express.Application = express()
-app.use(express.static(`${__dirname}/../docs`))
+import express from "express";
+import open from "open";
+import path from "path";
+const app: express.Application = express();
+app.use(express.static(path.join(__dirname, "../docs")));
 app.listen(process.env.npm_package_config_docViewPort, () => {
-    open(`http://localhost:${process.env.npm_package_config_docViewPort}/`)
-})
+  open(`http://localhost:${process.env.npm_package_config_docViewPort}/`);
+});
